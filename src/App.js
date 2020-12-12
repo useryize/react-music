895a7500601320c248f501ae0a1dccbaa6e75f6a
YIZE <136076063@qq.com>
2020-12-13 00:13:36 +0800

    router

--- src/App.js
+++ src/App.js
@@ -5,18 +5,16 @@ const { lazy, Suspense } = React;
 const Headers = lazy(() => import('./components/Header'));
 const Index = lazy(() => import('./components/List'));
 
-function App() {
-    return (
-        <BrowserRouter>
-            <Suspense fallback={null}>
-                <Switch>
-                    <Route path="/index" component={Index} />
-                    <Route path="/head" component={Headers} />
-                    <Redirect from='/' to='/index' />
-                </Switch>
-            </Suspense>
-        </BrowserRouter>
-    );
-}
+const App = () => (
+    <BrowserRouter>
+        <Suspense fallback={null}>
+            <Switch>
+                <Route path="/index" component={Index} />
+                <Route path="/head" component={Headers} />
+                <Redirect from='/' to='/index' />
+            </Switch>
+        </Suspense>
+    </BrowserRouter>
+)
 
 export default App;
