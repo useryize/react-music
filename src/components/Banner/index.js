8080eda15bedae1d0dc35b076834ab9306471ffc
YIZE <136076063@qq.com>
2020-12-14 22:39:03 +0800

    axios封装

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import styles from './index.module.css';
+import { get } from '../../utils/axios';
+const { useEffect } = React;
 
 const Banner = () => {
+    useEffect(() => {
+        get({url: 'list.json'})
+    }, []);
     return (
         <div className={styles.box}>798</div>
     )
