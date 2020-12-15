5184c878f2e49cb54f22814e496f0e176af1de1f
YIZE <136076063@qq.com>
2020-12-15 22:10:55 +0800

    hooks

--- src/pages/Find/index.js
+++ src/pages/Find/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import Banner from '../../components/Banner';
+import createContextFind from '../../hooks/Find/createContextFind';
 const Find = () => (
-    <>
+    <createContextFind.Provider value={{}}>
         <Banner />
-    </>
+    </createContextFind.Provider>
 );
 export default Find;
