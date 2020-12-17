ff8c43baf23994e992ec560bccb021f283bac2b7
hw21860065 <hw21860065@example.com>
2020-12-17 15:09:47 +0800

    banner Carousel

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,23 +1,46 @@
 import React from 'react';
 import createContextFind from '../../hooks/Find/createContextFind';
-import {findBannerList} from '../../hooks/Find/useReducerFind';
+import { findBannerList } from '../../hooks/Find/useReducerFind';
+import { Carousel, Button } from 'antd-mobile';
+import styles from './index.module.css';
 
-// import styles from './index.module.css';
-
-import { Button } from 'antd-mobile';
 const { useEffect, useContext } = React;
 
 const Banner = () => {
-    const { state, dispatch } = useContext(createContextFind);
+    const { state: {
+        list: {
+            banners = []
+        }
+    },
+        state,
+        dispatch
+    } = useContext(createContextFind);
     useEffect(() => {
         // other code
-        findBannerList({dispatch});
+        findBannerList({ dispatch });
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
     return (
-    <Button type="ghost" onClick={() => {
-      alert(JSON.stringify(state.list));
-    }}>123</Button>
+        <>
+            <Carousel
+
+                autoplay={false}
+                infinite
+                beforeChange={(from, to) => console.log(`slide from ${from} to ${to}`)}
+                afterChange={index => console.log('slide to', index)}
+            >
+                {
+                    banners && banners.map((item, index) => {
+                        return <div key={index}><img src={item.imageUrl} alt={item.typeTitle} />456</div>
+                    })
+                }
+                <Button onClick={() => {
+                    alert(JSON.stringify(state))
+                }}>789</Button>
+            </Carousel >
+            <div className={styles.box}>456</div>
+        </>
+
     )
 }
 
