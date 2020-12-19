155d67ff8229fc4a055a31b525f1a66b69027e6e
hw21860065 <hw21860065@example.com>
2020-12-19 09:20:32 +0800

    add vConsole

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './app';
 import './utils/flexible';
 import './assets/style/global.module.css';
+var VConsole = require('vconsole');
+var vConsole = new VConsole();
+console.error(vConsole);
+
 const Root = () => (
     <App />
 );
