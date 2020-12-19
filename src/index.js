d42faf4c2785f46f499ba2e398f848b213076246
hw21860065 <hw21860065@example.com>
2020-12-19 11:37:44 +0800

    axios

--- src/index.js
+++ src/index.js
@@ -4,8 +4,7 @@ import App from './app';
 import './utils/flexible';
 import './assets/style/global.module.css';
 var VConsole = require('vconsole');
-var vConsole = new VConsole();
-console.error(vConsole);
+new VConsole();
 
 const Root = () => (
     <App />
