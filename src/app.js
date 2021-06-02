a96399f9d52db8ba4c12d99c5cbb8d3d979ee649
hw21860065 <hw21860065@example.com>
2021-06-02 16:42:37 +0800

    banner目录切换

--- src/app.js
+++ src/app.js
@@ -3,12 +3,14 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 const { lazy, Suspense } = React;
 const Find = lazy(() => import('./pages/Find'));
+const Home = lazy(() => import('./pages/Home'));
 
 const App = () => (
     <BrowserRouter>
         <Suspense fallback={null}>
             <Switch>
                 <Route path="/find" component={Find} />
+                <Route path="/home" component={Home} />
                 <Route path="/" component={Find} />
                 {/* <Redirect from='/' to='/find' /> */}
             </Switch>
--- src/pages/Find/index.js
+++ src/pages/Find/index.js
@@ -1,11 +1,11 @@
-import React, {useReducer} from 'react';
-import Banner from '../../components/Banner';
+import React, { useReducer } from 'react';
+import Banner from './Banner';
 import createContextFind from '../../hooks/Find/createContextFind';
 import { reducer, initialState } from '../../hooks/Find/useReducerFind';
 const Find = () => {
-    const [state,dispatch] = useReducer(reducer, initialState);
+    const [state, dispatch] = useReducer(reducer, initialState);
     return (
-        <createContextFind.Provider value={{state, dispatch}}>
+        <createContextFind.Provider value={{ state, dispatch }}>
             <Banner />
         </createContextFind.Provider>
     )
