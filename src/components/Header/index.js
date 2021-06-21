74b18c2fdc8e6a7217ca89de63b67325d8b889d3
hw21860065 <hw21860065@example.com>
2021-06-21 18:31:45 +0800

    侧边栏

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useState, Fragment } from 'react';
+import { Drawer } from 'antd-mobile';
 import styles from './index.module.less';
 const Headers = () => {
-
+    let [searchTitle] = useState('hooks')
+    const sidebar = (
+        <div>2143</div>
+    )
     return (
-        <div className={styles.headBox}>
-            <div className={styles.left}>
-                <div className={styles.more}></div>
+        <Fragment>
+            <div className={styles.headBox}>
+                <div className={styles.left}>
+                    <div className={styles.more}></div>
+                </div>
+                <div className={styles.center}>{searchTitle}</div>
+                <div className={styles.right}></div>
             </div>
-            <div className={styles.center}></div>
-            <div className={styles.right}></div>
-        </div>
+            <Drawer
+                position="left"
+                open={true}
+                className={styles.sidebarStyle}
+                sidebar={sidebar}
+            />
+        </Fragment>
+
     )
 };
 
