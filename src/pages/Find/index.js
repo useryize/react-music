f93f8a63221a3cfde6f22514b88c171349adc706
useryize <hw21860065@example.com>
2021-06-22 15:49:35 +0800

    搜索

--- src/pages/Find/index.js
+++ src/pages/Find/index.js
@@ -1,13 +1,13 @@
 import React, { useReducer } from 'react';
 import Banner from './Banner';
-// import Header from '../../components/Header';
+import Header from '../../components/Header';
 import createContextFind from '../../hooks/Find/createContextFind';
 import { reducer, initialState } from '../../hooks/Find/useReducerFind';
 const Find = () => {
     const [state, dispatch] = useReducer(reducer, initialState);
     return (
         <createContextFind.Provider value={{ state, dispatch }}>
-            {/* <Header /> */}
+            <Header />
             <Banner />
         </createContextFind.Provider>
     )
