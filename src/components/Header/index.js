f78700b8227fa4074983f7ef1ad696bd3f436fe9
useryize <hw21860065@example.com>
2021-06-22 16:26:53 +0800

    头部搜索

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,26 +2,29 @@ import React, { useState, Fragment } from 'react';
 import { Drawer } from 'antd-mobile';
 import styles from './index.module.less';
 const Headers = () => {
-    let [searchTitle] = useState('hooks')
-    // const sidebar = (
-    //     <div>2143</div>
-    // )
+    let [searchTitle] = useState('hooks');
+    let [drawerShow, drawerShowFun] = useState(false);
+    const sidebar = (
+        <div>2143</div>
+    )
     return (
         <Fragment>
             <div className={styles.headBox}>
                 <div className={styles.left}>
-                    <div className={styles.more}></div>
+                    <div className={styles.more} onClick={() => drawerShowFun(!drawerShow)}></div>
                 </div>
                 <div className={styles.center}>{searchTitle}</div>
                 <div className={styles.right}></div>
             </div>
             <Drawer
                 position="left"
-                open={true}
+                open={drawerShow}
+                touch={false}
                 className={styles.sidebarStyle}
-                // sidebar={sidebar}
+                sidebar={sidebar}
+                onOpenChange={() => drawerShowFun(false)}
             >
-                <div>kjk</div>
+                <div></div>
             </Drawer>
         </Fragment>
 
