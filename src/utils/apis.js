cd336f7831298e061614175c8019bdba7ee9a74c
hw21860065 <hw21860065@example.com>
2021-10-13 13:56:30 +0800

    接口地址

--- src/utils/apis.js
+++ src/utils/apis.js
@@ -1,7 +1,8 @@
 const serverAddr = '47.115.57.59'
+const envDev = true
 export const SERVER = `http://${serverAddr}:3000`;
 
-export const bannerUrl = `/json/banner.json`; // banner
-// export const bannerUrl = `${SERVER}/banner`; // banner
+
+export const bannerUrl = envDev ? `/json/banner.json` : `${SERVER}/banner`; // banner
 export const personalized = `/json/paylist.json`; // 推荐歌单
 export const newsong = `/personalized/newsong`; // 最新音乐
