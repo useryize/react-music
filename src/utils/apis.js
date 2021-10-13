787c4154d3f4ae3e81bb93fb69d9f1914880906a
useryize <hw21860065@example.com>
2021-10-13 14:24:08 +0800

    接口地址

--- src/utils/apis.js
+++ src/utils/apis.js
@@ -3,6 +3,6 @@ const envDev = true
 export const SERVER = `http://${serverAddr}:3000`;
 
 
-export const bannerUrl = envDev ? `/json/banner.json` : `${SERVER}/banner`; // banner
+export const bannerUrl = envDev ? `/json/banner.json` : `${SERVER}/banner`; //  banner
 export const personalized = `/json/paylist.json`; // 推荐歌单
 export const newsong = `/personalized/newsong`; // 最新音乐
