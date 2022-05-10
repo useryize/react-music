f45d9eaf331b38a85f91a2de2ccc63253c662b2f
useryize <136076063@qq.com>
2022-05-10 14:57:27 +0800

    icon导航

--- src/pages/Find/index.js
+++ src/pages/Find/index.js
@@ -5,6 +5,7 @@ import { reducer, initialState } from '../../hooks/Find/useReducerFind';
 const Header = lazy(() => import('../../components/Header')); // 头部搜索
 const Banner = lazy(() => import('./Banner')); // banner
 const Recom = lazy(() => import('./Recom')); // 推荐歌单
+const Iconnav = lazy(() => import('./Iconnav')); // icon导航
 
 const Find = () => {
     const [state, dispatch] = useReducer(reducer, initialState);
@@ -12,6 +13,7 @@ const Find = () => {
         <createContextFind.Provider value={{ state, dispatch }}>
             <Header />
             <Banner />
+			<Iconnav />
             <Recom />
         </createContextFind.Provider>
     )
