41dd096f10e07b205a336f16363ed18804f8fed9
useryize <136076063@qq.com>
2022-05-10 15:20:32 +0800

    Update index.js

--- src/pages/Find/Iconnav/index.js
+++ src/pages/Find/Iconnav/index.js
@@ -1,8 +1,19 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import styles from './index.module.less'
 const Iconnav = () => {
+	const [iconList] = useState(Array.from({ length: 50 }))
+	console.error(iconList);
 	return (
-		<div>icon导航</div>
+		<div className={styles.iconBox}>
+			{
+				iconList.map((item) => (
+					<div className={styles.item} key={item && item.id}>
+						<div className={styles.img}></div>
+						<div className={styles.name}>4654</div>
+					</div>
+				))
+			}
+		</div>
 	)
 }
 
