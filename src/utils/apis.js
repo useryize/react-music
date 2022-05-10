038033ac6fe1a417f5ba9b0f3d159aec0f028d10
useryize <136076063@qq.com>
2022-05-10 16:49:06 +0800

    Update apis.js

--- src/utils/apis.js
+++ src/utils/apis.js
@@ -5,3 +5,4 @@ export const SERVER = 'https://useryize.vercel.app'
 export const bannerUrl = envDev ? `/json/banner.json` : `${SERVER}/banner`; // banner
 export const personalized = `/json/paylist.json`; // 推荐歌单
 export const newsong = `/personalized/newsong`; // 最新音乐
+export const homepageDragonBall = `${SERVER}/homepage/dragon/ball`; // 首页-发现-圆形图标入口列表
