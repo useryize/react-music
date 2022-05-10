0d8530e6a8bee07bb062e6075b80c2c32b0c0614
useryize <136076063@qq.com>
2022-05-10 10:54:20 +0800

    banner

--- src/pages/Find/Banner/index.js
+++ src/pages/Find/Banner/index.js
@@ -2,117 +2,124 @@ import React from 'react';
 import createContextFind from '../../../hooks/Find/createContextFind';
 import { findBannerList } from '../../../hooks/Find/useReducerFind';
 // import { Carousel } from 'antd-mobile';
-// import { Swiper } from 'antd-mobile'
-import Swiper, { Pagination } from "swiper";
+import { Swiper } from 'antd-mobile'
+// import Swiper, { Pagination } from "swiper";
 import "swiper/swiper-bundle.css";
 import styles from './index.module.less';
 
-Swiper.use([Pagination]);
+// Swiper.use([Pagination]);
 
 const {
-    useEffect,
-    useContext,
-    // useRef,
+	useEffect,
+	useContext,
+	// useRef,
 } = React;
 
 
 const Banner = () => {
-    // const swiperDom = useRef(null);
-    const { state: {
-        bannerList: {
-            banners = []
-        }
-    },
-        // state,
-        dispatch
-    } = useContext(createContextFind);
-    useEffect(() => {
-        // other code
-        findBannerList({ dispatch });
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+	// const swiperDom = useRef(null);
+	const { state: {
+		bannerList: {
+			banners = []
+		}
+	},
+		// state,
+		dispatch
+	} = useContext(createContextFind);
+	useEffect(() => {
+		// other code
+		findBannerList({ dispatch });
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
 
-    useEffect(() => {
-        let swiper = new Swiper('.swiper-container-banner', {
-            loop: true,
-            pagination: {
-                el: '.swiper-pagination',
-            }
-        });
-        return () => {
-            swiper.destroy();
-        }
-    }, [banners]);
-    // return (
-    //     <Swiper>
-    //         {
-    //             banners.map((item, index) => (
-    //                 <Swiper.Item key={index}>{item.pic}</Swiper.Item>
-    //             ))
-    //         }
-    //     </Swiper>
-    // )
-    return (
-        <div className={`swiper-container swiper-container-banner ${styles.bannerBox}`}>
-            <div className="swiper-wrapper">
-                {
-                    banners.map((item, index) => {
-                        return (
-                            <div
-                                className={`swiper-slide ${styles.banner}`} key={index}
-                                style={{ backgroundImage: `url(${item.pic})` }}
-                            >
-                                <div className={styles.tigs}>{item.typeTitle}</div>
-                            </div>
-                        )
-                    })
-                }
-            </div>
-            <div className="swiper-pagination"></div>
-        </div>
-    )
-    // return (
-    //     banners && banners.length > 0 &&
-    //     <div className={styles.bannerBox}>
-    //         <Carousel
-    //             autoplay
-    //             infinite
-    //         >
-    //             {
-    //                 banners && banners.map((item, index) => {
-    //                     return (
-    //                         <div
-    //                             className={styles.banner} key={index}
-    //                             style={{ backgroundImage: `url(${item.pic})` }}
-    //                         >
-    //                             {/* <img className={styles.imgBox} src={item.pic} alt={item.typeTitle} /> */}
-    //                         </div>
-    //                     )
-    //                 })
-    //             }
-    //         </Carousel>
-    //     </div>
-    // )
-    // return (
-    //     <div ref={swiperDom} className={`${styles.bannerBox} swiper-container`}>
-    //         <div className="swiper-wrapper">
-    //             {
-    //                 banners && banners.map((item, index) => {
-    //                     return (
-    //                         <div className="swiper-slide" key={index}>
-    //                             <div key={index} className={`${styles.carouselBox} `}>
-    //                                 <div className={styles.imgBox}>
-    //                                     <img className={styles.img} src={item.pic} alt={item.typeTitle} />
-    //                                 </div>
-    //                             </div>
-    //                         </div>
+	// useEffect(() => {
+	//     let swiper = new Swiper('.swiper-container-banner', {
+	//         loop: true,
+	//         pagination: {
+	//             el: '.swiper-pagination',
+	//         }
+	//     });
+	//     return () => {
+	//         swiper.destroy();
+	//     }
+	// }, [banners]);
+	return (
+		<div className={styles.bannerBox}>
+			<Swiper>
+				{
+					banners.map((item, index) => (
+						<Swiper.Item key={index}>
+							<div className={styles.item} style={{ backgroundImage: `url(${item.pic})` }}>
+								<div className={styles.tigs}>{item.typeTitle}</div>
+							</div>
+						</Swiper.Item>
+					))
+				}
+			</Swiper>
+		</div>
 
-    //                     )
-    //                 })
-    //             }
-    //         </div>
-    //     </div>
-    // )
+	)
+	// return (
+	//     <div className={`swiper-container swiper-container-banner ${styles.bannerBox}`}>
+	//         <div className="swiper-wrapper">
+	//             {
+	//                 banners.map((item, index) => {
+	//                     return (
+	//                         <div
+	//                             className={`swiper-slide ${styles.banner}`} key={index}
+	//                             style={{ backgroundImage: `url(${item.pic})` }}
+	//                         >
+	//                             <div className={styles.tigs}>{item.typeTitle}</div>
+	//                         </div>
+	//                     )
+	//                 })
+	//             }
+	//         </div>
+	//         <div className="swiper-pagination"></div>
+	//     </div>
+	// )
+	// return (
+	//     banners && banners.length > 0 &&
+	//     <div className={styles.bannerBox}>
+	//         <Carousel
+	//             autoplay
+	//             infinite
+	//         >
+	//             {
+	//                 banners && banners.map((item, index) => {
+	//                     return (
+	//                         <div
+	//                             className={styles.banner} key={index}
+	//                             style={{ backgroundImage: `url(${item.pic})` }}
+	//                         >
+	//                             {/* <img className={styles.imgBox} src={item.pic} alt={item.typeTitle} /> */}
+	//                         </div>
+	//                     )
+	//                 })
+	//             }
+	//         </Carousel>
+	//     </div>
+	// )
+	// return (
+	//     <div ref={swiperDom} className={`${styles.bannerBox} swiper-container`}>
+	//         <div className="swiper-wrapper">
+	//             {
+	//                 banners && banners.map((item, index) => {
+	//                     return (
+	//                         <div className="swiper-slide" key={index}>
+	//                             <div key={index} className={`${styles.carouselBox} `}>
+	//                                 <div className={styles.imgBox}>
+	//                                     <img className={styles.img} src={item.pic} alt={item.typeTitle} />
+	//                                 </div>
+	//                             </div>
+	//                         </div>
+
+	//                     )
+	//                 })
+	//             }
+	//         </div>
+	//     </div>
+	// )
 }
 
 export default Banner;
