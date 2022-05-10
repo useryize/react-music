2d600119ae9c34c78b7adb3ad84716293a869861
useryize <136076063@qq.com>
2022-05-10 16:48:27 +0800

    Update index.js

--- src/pages/Find/Iconnav/index.js
+++ src/pages/Find/Iconnav/index.js
@@ -1,19 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
+import createContextFind from '../../../hooks/Find/createContextFind'
+import { getFindIconNav } from '../../../hooks/Find/useReducerFind'
 import styles from './index.module.less'
 const Iconnav = () => {
+	const { state: { iconNavList = '' } = {}, dispatch } = useContext(createContextFind)
 	const [iconList] = useState(Array.from({ length: 50 }))
-	console.error(iconList);
+	useEffect(() => {
+		getFindIconNav({ dispatch })
+	}, [])
 	return (
-		<div className={styles.iconBox}>
-			{
-				iconList.map((item) => (
-					<div className={styles.item} key={item && item.id}>
-						<div className={styles.img}></div>
-						<div className={styles.name}>4654</div>
-					</div>
-				))
-			}
-		</div>
+		<>
+			<div className={styles.iconBox} onClick={() => getFindIconNav({ dispatch })}>
+				{
+					iconList.map((item, index) => (
+						<div className={styles.item} key={(item && item.id) || index}>
+							<div className={styles.img}></div>
+							<div className={styles.name}>4654</div>
+						</div>
+					))
+				}
+			</div>
+			<div>
+				{JSON.stringify(iconNavList)}
+			</div>
+		</>
+
 	)
 }
 
