75e42798113e9318254bf1998c7aaac3d6414b2c
useryize <136076063@qq.com>
2022-05-11 14:58:15 +0800

    Update axios.js

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,28 +1,45 @@
 import axios from 'axios';
 
-axios.defaults.headers.get['Content-Type'] = 'application/x-www-form-urlencoded';
+// axios.defaults.headers.get['Content-Type'] = 'application/x-www-form-urlencoded';
 // axios.defaults.headers.get['Accept'] = 'application/json; charset=utf-8';
 // axios.defaults.headers.get['Content-Security-Policy'] = 'upgrade-insecure-requests';
 // axios.defaults.headers.get['Access-Control-Allow-Headers'] = 'Content-Type,XFILENAME,XFILECATEGORY,XFILESIZE';
 axios.defaults.withCredentials = true;
 
-axios.interceptors.response.use(
-    function (response) {
-        return response;
+// 请求拦截器
+axios.interceptors.request.use(
+    (config) => {
+        config.headers = {
+            'Content-Type': 'application/json'
+        }
+        return config
     },
-    function (error) {
+    (error) => {
+        return Promise.reject(error)
+    }
+)
 
+// 响应了拦截器
+axios.interceptors.response.use(
+    (response) => {
+        return response
     },
-);
+    (error) => {
+        // console.log(error.config);
+        // console.log(error.response);
+        return Promise.reject(error)
+    }
+)
 
 export const axiosGet = ({ url, prm }) => {
     return new Promise((resolve, reject) => {
         axios.get(url, { params: prm })
             .then(res => {
-                resolve(res.data);
+                resolve(res && res.data);
             })
             .catch(err => {
-                reject(err.data);
+                console.error(err);
+                // reject(err.data);
             })
     });
 }
