fb46cf32e3e190af33a0e1012a5ed71fd5a9a432
useryize <136076063@qq.com>
2022-05-13 11:32:53 +0800

    Update index.js

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,18 @@
-import React, { useState, Fragment } from 'react';
+import React, { useState, Fragment, useContext } from 'react';
+import createContextFind from '../../hooks/Find/createContextFind';
 import { Popup, Avatar } from 'antd-mobile';
 import styles from './index.module.less';
 const Headers = () => {
+    const { props } = useContext(createContextFind)
     let [searchTitle] = useState('hooks');
     let [drawerShow, drawerShowFun] = useState(false);
+    const toLogin = () => {
+        const { history } = props;
+        history.push({
+            pathname: "/login",
+        });
+
+    }
     return (
         <Fragment>
             <div className={styles.headBox}>
@@ -18,7 +27,7 @@ const Headers = () => {
                     />
                 </div>
                 <div className={styles.center}>{searchTitle}</div>
-                <div className={styles.right}>立即登录</div>
+                <div className={styles.right} onClick={toLogin}>立即登录</div>
             </div>
             <Popup
                 position="left"
