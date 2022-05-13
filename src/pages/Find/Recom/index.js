e3754167221d6fc0b2fb7d4b6907c3b8c65583fb
useryize <136076063@qq.com>
2022-05-13 16:23:11 +0800

    推荐歌单

--- src/pages/Find/Recom/index.js
+++ src/pages/Find/Recom/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import Swiper from 'swiper';
+// import Swiper from 'swiper';
 import styles from './index.module.less';
 import { findRecomList } from '../../../hooks/Find/useReducerFind';
 
 import createContextFind from '../../../hooks/Find/createContextFind';
-const { useContext, useEffect } = React;
+const {
+    useContext,
+    useEffect,
+    useState,
+} = React;
 const Recom = () => {
     const {
         state: {
@@ -14,55 +18,34 @@ const Recom = () => {
         } = {},
         dispatch
     } = useContext(createContextFind);
-
+    const [list] = useState(Array.from({ length: 20 }).map(item => ({ picUrl: '', name: 'XXX' })))
     useEffect(() => {
-        //  other code
         findRecomList({ dispatch });
-        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
-    useEffect(() => {
-        let swiper = new Swiper('.swiper-container-recom', {
-            slidesPerView: 3,
-            spaceBetween: 30,
-        });
-        return () => {
-            swiper.destroy();
-        }
-    }, [playlists]);
+    console.error(list);
+    // useEffect(() => {
+    //     let swiper = new Swiper('.swiper-container-recom', {
+    //         slidesPerView: 3,
+    //         spaceBetween: 30,
+    //     });
+    //     return () => {
+    //         swiper.destroy();
+    //     }
+    // }, [playlists]);
     return (
-
-
-        //         <div class="swiper-container">
-        //     <div class="swiper-wrapper">
-        //       <div class="swiper-slide">Slide 1</div>
-        //       <div class="swiper-slide">Slide 2</div>
-        //       <div class="swiper-slide">Slide 3</div>
-        //       <div class="swiper-slide">Slide 4</div>
-        //       <div class="swiper-slide">Slide 5</div>
-        //       <div class="swiper-slide">Slide 6</div>
-        //       <div class="swiper-slide">Slide 7</div>
-        //       <div class="swiper-slide">Slide 8</div>
-        //       <div class="swiper-slide">Slide 9</div>
-        //       <div class="swiper-slide">Slide 10</div>
-        //     </div>
-        //     <!-- Add Pagination -->
-        //     <div class="swiper-pagination"></div>
-        //   </div>
         <div className={styles.recomBox}>
             <div className={styles.title}>推荐歌单</div>
-            <div className={`swiper-container swiper-container-recom ${styles.itemBox}`}>
-                <div className="swiper-wrapper">
-                    {
-                        playlists.map(item => (
-                            <div className={`swiper-slide ${styles.item}`} key={item.id}>
-                                <div className={styles.pic}>
-                                    <img src={item.coverImgUrl} alt="" />
-                                </div>
-                                <div className={styles.name}>{item.copywriter}</div>
+            <div className={styles.itemBox}>
+                {
+                    list.map((item, index) => (
+                        <div className={styles.item} key={index}>
+                            <div className={styles.pic}>
+                                <img src={item.picUrl} alt="" />
                             </div>
-                        ))
-                    }
-                </div>
+                            <div className={styles.name}>{item.name}</div>
+                        </div>
+                    ))
+                }
 
             </div>
 
