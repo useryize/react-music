2d6560b4070a63db913d3a97017e726339ba0598
useryize <136076063@qq.com>
2022-05-13 16:22:21 +0800

    推荐歌单

--- src/utils/apis.js
+++ src/utils/apis.js
@@ -4,6 +4,7 @@ export const SERVER = 'https://useryize.vercel.app'
 
 export const loginCellphone = '/login/cellphone'; // login
 export const bannerUrl = envDev ? `/json/banner.json` : `${SERVER}/banner`; // banner
-export const personalized = `/json/paylist.json`; // 推荐歌单
-export const newsong = `/personalized/newsong`; // 最新音乐
 export const homepageDragonBall = envDev ? `/json/ball.json` : `${SERVER}/homepage/dragon/ball`; // 首页-发现-圆形图标入口列表
+export const personalized = `${SERVER}/personalized` // 推荐歌单
+export const newsong = `/personalized/newsong`; // 最新音乐
+
