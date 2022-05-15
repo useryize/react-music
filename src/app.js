705f40fc1d3fe2faa8052d7c98085cc6f0f80688
易泽 <136076063@qq.com>
2022-05-15 20:31:14 +0800

    搜索页

--- src/app.js
+++ src/app.js
@@ -9,20 +9,24 @@ const Login = lazy(() => import('./pages/Login'));
 const SongSheetDetails = lazy(() => import('./pages/SongSheetDetails'));
 const Search = lazy(() => import('./pages/Search'));
 
-const App = () => (
-    <HashRouter history={history}>
-        <Suspense fallback={null}>
-            <Switch>
-                <Route path="/find" component={Find} />
-                <Route path="/home" component={Home} />
-                <Route path="/login" component={Login} />
-                <Route path="/songSheetDetails/:id" component={SongSheetDetails} />
-                <Route path="/search" component={Search} />
-                <Route path="/" component={Find} />
-                {/* <Redirect from='/' to='/find' /> */}
-            </Switch>
-        </Suspense>
-    </HashRouter>
-)
+const App = (props) => {
+    return (
+        <>
+            <HashRouter history={history}>
+                <Suspense fallback={null}>
+                    <Switch>
+                        <Route path="/find" component={Find} />
+                        <Route path="/home" component={Home} />
+                        <Route path="/login" component={Login} />
+                        <Route path="/songSheetDetails/:id" component={SongSheetDetails} />
+                        <Route path="/search" component={Search} />
+                        <Route path="/" component={Find} />
+                        {/* <Redirect from='/' to='/find' /> */}
+                    </Switch>
+                </Suspense>
+            </HashRouter>
+        </>
+    )
+}
 
 export default App;
