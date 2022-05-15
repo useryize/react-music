e27bebe11ddc9dc2589b98624efe5a82c8ec48c2
易泽 <136076063@qq.com>
2022-05-15 18:24:33 +0800

    搜索歌单

--- src/pages/Search/SearchList/index.js
+++ src/pages/Search/SearchList/index.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import createContextSearch from '../../../hooks/Search/createContextSearch'
 import { getSearch } from '../../../hooks/Search/useReducerSearch'
-import { List, Image } from 'antd-mobile'
+import { Image } from 'antd-mobile'
 import history from '../../../utils/history'
+import styles from './index.module.less'
+import { PlayOutline } from 'antd-mobile-icons'
+import { formatNumber } from '@/utils/utils'
 const {
     useContext,
     useEffect
@@ -28,24 +31,48 @@ const KeywordTabs = () => {
         });
     }
     return (
-        <List>
+        // <List>
+        //     {
+        //         playlists.map(item => (
+        //             <List.Item key={item.id} onClick={() => {
+        //                 toSongSheetDetails(item)
+        //             }}>
+        //                 <Image
+        //                     width='0.8rem'
+        //                     heigth='0.8rem'
+        //                     fit="cover"
+        //                     lazy={true}
+        //                     src={item.coverImgUrl}
+        //                 ></Image>
+        //                 <div>{item.name}</div>
+        //             </List.Item>
+        //         ))
+        //     }
+        // </List>
+        <div className={styles.listBox}>
             {
-                playlists.map(item => (
-                    <List.Item key={item.id} onClick={() => {
+                playlists.map((item, index) => (
+                    <div className={styles.item} key={index} onClick={() => {
                         toSongSheetDetails(item)
                     }}>
-                        <Image
-                            width='0.8rem'
-                            heigth='0.8rem'
-                            fit="cover"
-                            lazy={true}
-                            src={item.coverImgUrl}
-                        ></Image>
-                        <div>{item.name}</div>
-                    </List.Item>
+                        <div className={styles.pic}>
+                            <Image
+                                width='100%'
+                                heigth='auto'
+                                fit="cover"
+                                lazy={true}
+                                src={item.coverImgUrl}
+                            ></Image>
+                            <div className={styles.playCount}>
+                                <PlayOutline />
+                                <span>{formatNumber(item.playCount)}</span>
+                            </div>
+                        </div>
+                        <div className={styles.name}>{item.name}</div>
+                    </div>
                 ))
             }
-        </List>
+        </div>
     )
 }
 export default KeywordTabs
