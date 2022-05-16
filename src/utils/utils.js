b4963e8583aacd5f175f53d87914a28682fd5669
useryize <136076063@qq.com>
2022-05-16 18:26:58 +0800

    发布-订阅 监听缓存

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -2,8 +2,9 @@ export const formatNumber = (number) => {
     return (number / 10000).toFixed(2) + '万';
 }
 
-/**
- * 缓存歌曲信息 并监听缓存值
+/**发布-订阅 监听缓存
+ * 
+ * 缓存歌曲信息
  * 要监听同一页面的缓存值 调用songInfoLocalStorage.setItem
  * 监听自定义事件songInfoSetItemEvent
  * 
@@ -15,10 +16,10 @@ export const songInfoLocalStorage = (info = {}) => {
     const originStorage = window.localStorage;
     const storage = {
         setItem: function () {
-            let setItemEvent = new Event('songInfoSetItemEvent');
-            setItemEvent.key = 'songData';
+            let setItemEvent = new Event('songInfoSetItemEvent', { bubbles: false, cancelable: true, composed: false });
+            setItemEvent.info = info
             window.dispatchEvent(setItemEvent);
-            storage.setItem('songData', JSON.stringify(info))
+            originStorage.setItem('songData', JSON.stringify(info))
         },
         getItem: function () {
             return JSON.parse(originStorage.getItem('songData'));
