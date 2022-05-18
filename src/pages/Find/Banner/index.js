6b3371bf41d87e063b2a4dd7a0f305f1f1b4aaf1
useryize <136076063@qq.com>
2022-05-18 14:28:54 +0800

    基础尺寸更改

--- src/pages/Find/Banner/index.js
+++ src/pages/Find/Banner/index.js
@@ -9,57 +9,57 @@ import styles from './index.module.less';
 // Swiper.use([Pagination]);
 
 const {
-	useEffect,
-	useContext,
+    useEffect,
+    useContext,
 } = React;
 
 
 const Banner = () => {
-	const { state: {
-		bannerList: {
-			banners = []
-		} = {}
-	},
-		dispatch
-	} = useContext(createContextFind);
-	useEffect(() => {
-		findBannerList({ dispatch });
-	}, []);
+    const { state: {
+        bannerList: {
+            banners = []
+        } = {}
+    },
+        dispatch
+    } = useContext(createContextFind);
+    useEffect(() => {
+        findBannerList({ dispatch });
+    }, []);
 
-	if (banners.length === 0) {
-		return <div className={styles.emptyBanner}>加载中...</div>
-	}
+    if (banners.length === 0) {
+        return <div className={styles.emptyBanner}>加载中...</div>
+    }
 
-	return (
-		<div className={styles.bannerBox}>
-			<Swiper
-				loop={true}
-				rubberband={true}
-				stuckAtBoundary={true}
-				indicatorProps={{
-					style: {
-						'--dot-color': 'rgba(0, 0, 0, 0.4)',
-						'--active-dot-color': '#ffffff',
-						'--dot-size': '.1rem',
-						'--active-dot-size': '.3rem',
-						'--dot-border-radius': '50%',
-						'--active-dot-border-radius': '.15rem',
-						'--dot-spacing': '.08rem',
-					}
-				}}
-			>
-				{
-					banners.map((item, index) => (
-						<Swiper.Item key={index}>
-							<div className={styles.banner} style={{ backgroundImage: `url(${item.pic})` }}>
-								<div className={styles.tigs}>{item.typeTitle}</div>
-							</div>
-						</Swiper.Item>
-					))
-				}
-			</Swiper>
-		</div>
-	)
+    return (
+        <div className={styles.bannerBox}>
+            <Swiper
+                loop={true}
+                rubberband={true}
+                stuckAtBoundary={true}
+                indicatorProps={{
+                    style: {
+                        '--dot-color': 'rgba(0, 0, 0, 0.4)',
+                        '--active-dot-color': '#ffffff',
+                        '--dot-size': '.05rem',
+                        '--active-dot-size': '.15rem',
+                        '--dot-border-radius': '50%',
+                        '--active-dot-border-radius': '.8rem',
+                        '--dot-spacing': '.04rem',
+                    }
+                }}
+            >
+                {
+                    banners.map((item, index) => (
+                        <Swiper.Item key={index}>
+                            <div className={styles.banner} style={{ backgroundImage: `url(${item.pic})` }}>
+                                <div className={styles.tigs}>{item.typeTitle}</div>
+                            </div>
+                        </Swiper.Item>
+                    ))
+                }
+            </Swiper>
+        </div>
+    )
 }
 
 export default Banner;
