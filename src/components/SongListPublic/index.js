ddfeedc91b6b644021ef9bdc94584e43b7f2d236
useryize <136076063@qq.com>
2022-05-18 17:58:37 +0800

    Update index.js

--- src/components/SongListPublic/index.js
+++ src/components/SongListPublic/index.js
@@ -2,20 +2,21 @@ import React from 'react';
 import createContextApp from '../../hooks/App/createContextApp'
 import { getSongUrl, singleInfoFunction } from '../../hooks/App/useReducerApp'
 import { List, Image } from "antd-mobile";
+import styles from 'index.module.less'
+import { PlayOutline } from 'antd-mobile-icons'
 const {
     useContext,
 } = React
 
 const SongListPublic = (_props) => {
-    console.error(_props);
     const { dataInfo = [] } = _props
     // app数据
     const { dispatch } = useContext(createContextApp)
     return (
-        <List>
+        <div className={styles.songBox}>
             {
-                dataInfo.map(item => (
-                    <List.Item key={item.id} onClick={async () => {
+                dataInfo.map((item, index) => (
+                    <div className={styles.item} key={index} onClick={async () => {
                         const res = await getSongUrl({ dispatch, params: { id: item.id } }) // 获取音乐id
                         const { data: [obj = {}] = [] } = res || {}
                         const songObj = {
@@ -26,20 +27,45 @@ const SongListPublic = (_props) => {
                         singleInfoFunction({ dispatch, params: songObj })
                         // songInfoLocalStorage(songObj).setItem() // 缓存音乐信息
                     }}>
-                        <Image
-                            width='0.8rem'
-                            heigth='0.8rem'
-                            fit="cover"
-                            lazy={true}
-                            src={item.al && item.al.picUrl}
-                        ></Image>
-                        <div>{item.name}</div>
-                        <div>{item.al.name}</div>
-                    </List.Item>
+                        <div className={styles.left}>
+                            <div className={styles.order}>{index + 1}</div>
+                            <div className={styles.titleBox}>
+                                <div className={styles.title}>{item.name}</div>
+                                <div className={styles.doc}>
+                                    <span className={styles.vip}>
+                                        {
+                                            [1, 8].includes(+item.fee) && 'vip' || 'SQ'
+                                        }
+                                    </span>
+                                    <span>{item.al.name}</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={styles.right}>
+                            <PlayOutline fontSize='.14rem' />
+                        </div>
+                    </div>
                 ))
-            }
 
-        </List>
+            }
+        </div >
+        // <List>
+        //     {
+        //         dataInfo.map(item => (
+        //             <List.Item key={item.id}}>
+        //                 <Image
+        //                     width='0.8rem'
+        //                     heigth='0.8rem'
+        //                     fit="cover"
+        //                     lazy={true}
+        //                     src={item.al && item.al.picUrl}
+        //                 ></Image>
+        //                 <div>{item.name}</div>
+        //                 <div>{item.al.name}</div>
+        //             </List.Item>
+        //         ))
+        //     }
+        // </List>
     )
 }
 
