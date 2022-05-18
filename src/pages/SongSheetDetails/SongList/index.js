09ee57adb9f098cd1d1e1470d87dcfd1e0d6e6a0
useryize <136076063@qq.com>
2022-05-18 17:27:24 +0800

    Update index.js

--- src/pages/SongSheetDetails/SongList/index.js
+++ src/pages/SongSheetDetails/SongList/index.js
@@ -1,45 +1,48 @@
-import React from 'react';
-import createContextApp from '../../hooks/App/createContextApp'
-import { getSongUrl, singleInfoFunction } from '../../hooks/App/useReducerApp'
-import { List, Image } from "antd-mobile";
+import React from "react";
+import createContextSong from '@/hooks/SongSheetDetails/createContextSong'
+import { getPlaylistDetail } from '@/hooks/SongSheetDetails/useReducerSong'
+import SongListPublic from '../../../components/SongListPublic'
+import styles from './index.module.less'
 const {
     useContext,
+    useEffect,
 } = React
+const SongListCom = () => {
 
-const SongListPublic = (_props) => {
-    const { dataInfo = [] } = _props
-    // app数据
-    const { dispatch } = useContext(createContextApp)
+    const {
+        state: {
+            songList: {
+                playlist: {
+                    tracks = []
+                } = {},
+                playlist = {}
+            } = {},
+        } = {}, dispatch, props
+    } = useContext(createContextSong)
+
+    useEffect(() => {
+        // 歌单列表
+        const { match: { params: { id = '' } = {} } = {} } = props
+        getPlaylistDetail({ dispatch, params: { id } })
+    }, [])
+
     return (
-        <List>
-            {
-                dataInfo.map(item => (
-                    <List.Item key={item.id} onClick={async () => {
-                        const res = await getSongUrl({ dispatch, params: { id: item.id } }) // 获取音乐id
-                        const { data: [obj = {}] = [] } = res || {}
-                        const songObj = {
-                            mp3Url: obj && obj.url,
-                            mp3Pic: (item && item.al && item.al.picUrl) || '',
-                            mp3Name: item.name
-                        }
-                        singleInfoFunction({ dispatch, params: songObj })
-                        // songInfoLocalStorage(songObj).setItem() // 缓存音乐信息
-                    }}>
-                        <Image
-                            width='0.8rem'
-                            heigth='0.8rem'
-                            fit="cover"
-                            lazy={true}
-                            src={item.al && item.al.picUrl}
-                        ></Image>
-                        <div>{item.name}</div>
-                        <div>{item.al.name}</div>
-                    </List.Item>
-                ))
-            }
-
-        </List>
+        <>
+            <div className={styles.songPicBox}>
+                <div className={styles.songBlur} style={{ backgroundImage: `url(${playlist.coverImgUrl})` }}></div>
+                <div className={styles.songpic}>
+                    <div className={styles.left}>
+                        <img src={playlist.coverImgUrl} alt="" />
+                    </div>
+                    <div className={styles.right}>
+                        <div className={styles.title}>{playlist.name}sadasdasdasd</div>
+                        <div className={styles.doc}>{playlist.description}asdasdasdasd</div>
+                    </div>
+                </div>
+            </div>
+            <SongListPublic dataInfo={tracks} />
+        </>
     )
 }
 
-export default SongListPublic;
+export default SongListCom
