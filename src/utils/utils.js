4103cc03c78d839cda63afa2f0ae014918c5e0a3
useryize <136076063@qq.com>
2022-05-19 17:46:35 +0800

    JS颜色十六进制转换为rgb或rgba

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -26,4 +26,33 @@ export const songInfoLocalStorage = (info = {}) => {
         }
     }
     return storage
-} 
+}
+
+/**
+ * JS颜色十六进制转换为rgb或rgba
+ * 
+ * @param {*} sColor #ffffff
+ * @param {*} opacity 1
+ * @returns #ffffff => reba(255,255,255,1)
+ */
+export const colorRgb = (sColor, opacity) => {
+    let reg = /^#([0-9a-fA-f]{3}|[0-9a-fA-f]{6})$/; /*16进制颜色转为RGB格式*/
+    sColor = sColor.toLowerCase();
+    if (sColor && reg.test(sColor)) {
+        if (sColor.length === 4) {
+            var sColorNew = "#";
+            for (var i = 1; i < 4; i += 1) {
+                sColorNew += sColor.slice(i, i + 1).concat(sColor.slice(i, i + 1));
+            }
+            sColor = sColorNew;
+        }
+
+        //处理六位的颜色值  
+        let sColorChange = [];
+        for (let i = 1; i < 7; i += 2) {
+            sColorChange.push(parseInt("0x" + sColor.slice(i, i + 2)));
+        }
+        return `rgba(${sColorChange.join(",")},${opacity})`
+    }
+    return sColor;
+}
