c970e41f2f9aca9ec2e9c03f8a0ca055df5e8eb4
useryize <136076063@qq.com>
2022-05-20 15:43:36 +0800

    歌曲列表

--- src/components/SongListPublic/index.js
+++ src/components/SongListPublic/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import createContextApp from '../../hooks/App/createContextApp'
-import { getSongUrl, singleInfoFunction } from '../../hooks/App/useReducerApp'
+import { getSongIdApp } from '../../hooks/App/useReducerApp'
 // import { List, Image } from "antd-mobile";
 import styles from './index.module.less'
 import { PlayOutline } from 'antd-mobile-icons'
@@ -21,18 +21,10 @@ const SongListPublic = (_props) => {
                 dataInfo.map((item, index) => (
                     <div className={styles.item} key={index} onClick={async () => {
                         setSongFocu(index)
-                        const res = await getSongUrl({ dispatch, params: { id: item.id } }) // 获取音乐id
-                        const { data: [obj = {}] = [] } = res || {}
-                        const songObj = {
-                            mp3Url: obj && obj.url,
-                            mp3Pic: (item && item.al && item.al.picUrl) || '',
-                            mp3Name: item.name
-                        }
-                        singleInfoFunction({ dispatch, params: songObj })
-                        // songInfoLocalStorage(songObj).setItem() // 缓存音乐信息
+                        getSongIdApp({ dispatch, params: item.id })
                     }}>
                         <div className={styles.left}>
-                            <div className={`${styles.order} ${index === songFocu ? styles.focu : ''}` }>{index + 1}</div>
+                            <div className={`${styles.order} ${index === songFocu ? styles.focu : ''}`}>{index + 1}</div>
                             <div className={styles.titleBox}>
                                 <div className={styles.title}>{item.name}</div>
                                 <div className={styles.doc}>
@@ -58,23 +50,6 @@ const SongListPublic = (_props) => {
 
             }
         </div >
-        // <List>
-        //     {
-        //         dataInfo.map(item => (
-        //             <List.Item key={item.id}}>
-        //                 <Image
-        //                     width='0.8rem'
-        //                     heigth='0.8rem'
-        //                     fit="cover"
-        //                     lazy={true}
-        //                     src={item.al && item.al.picUrl}
-        //                 ></Image>
-        //                 <div>{item.name}</div>
-        //                 <div>{item.al.name}</div>
-        //             </List.Item>
-        //         ))
-        //     }
-        // </List>
     )
 }
 
