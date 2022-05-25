8786efe1ad9eda5b80e1de94646452c92c3072e7
useryize <136076063@qq.com>
2022-05-25 18:10:03 +0800

    Update index.js

--- src/pages/Find/index.js
+++ src/pages/Find/index.js
@@ -2,7 +2,7 @@ import React, { useReducer, lazy } from 'react';
 import createContextFind from '../../hooks/Find/createContextFind';
 import { reducer, initialState } from '../../hooks/Find/useReducerFind';
 
-const Header = lazy(() => import('../../components/Header')); // 头部搜索
+// const Header = lazy(() => import('../../components/Header')); // 头部搜索
 const Banner = lazy(() => import('./Banner')); // banner
 const Recom = lazy(() => import('./Recom')); // 推荐歌单
 const Iconnav = lazy(() => import('./Iconnav')); // icon导航
@@ -11,7 +11,7 @@ const Find = (props) => {
     const [state, dispatch] = useReducer(reducer, initialState);
     return (
         <createContextFind.Provider value={{ state, dispatch, props }}>
-            <Header />
+//             <Header />
             <Banner />
             <Iconnav />
             <Recom />
