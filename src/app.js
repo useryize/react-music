28b895bf16c9b2707c62baef20fd0192dac8e4b6
useryize <136076063@qq.com>
2022-05-25 10:59:53 +0800

    react-router-dom v6

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { HashRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes  } from 'react-router-dom';
 import createContextFind from './hooks/App/createContextApp'
 import { reducer, initialState } from './hooks/App/useReducerApp'
 import history from './utils/history';
-import routes from './routes'
+import Aaa from './components/Player'
+// import routes from './routes'
 const { lazy, Suspense, useReducer, useState } = React;
+
 const Player = lazy(() => import('./components/Player'));
 const Header = lazy(() => import('./components/Header/header'));
 
@@ -12,40 +14,45 @@ const App = () => {
     const [state, dispatch] = useReducer(reducer, initialState)
     const [heaterTitle, setHeaterTitle] = useState(null)
     return (
-        <createContextFind.Provider value={{ state, dispatch }}>
-            <Suspense fallback={
-                null
-                // <div>Loading...</div> 
-            }>
-                <Header heaterTitle={heaterTitle} />
-                <HashRouter history={history}>
+		<Router>
+			<Routes>
+				<Route path='home' element={<Aaa></Aaa>}></Route>
+			</Routes>
+		</Router>
+        // <createContextFind.Provider value={{ state, dispatch }}>
+        //     <Suspense fallback={
+        //         null
+        //         // <div>Loading...</div> 
+        //     }>
+        //         <Header heaterTitle={heaterTitle} />
+        //         <HashRouter history={history}>
 
-                    <Switch>
-                        {
-                            routes.map(item => {
-                                const { path, exact = true, component: C } = item;
-                                return <Route
-                                    key={path}
-                                    path={path}
-                                    exact={exact}
-                                    // component={C}
-                                    render={(props) => {
-                                        // props.history.title = item.title
-                                        // window.reactMusicTitle = item.title
-                                        setHeaterTitle(item)
-                                        return <C {...props} key={item.title} title={item.title} />
-                                    }}
-                                />
-                            })
-                        }
-                        <Route path="*" render={(props) => {
-                            props.history.push({ pathname: "/find", })
-                        }} />
-                    </Switch>
-                </HashRouter>
-                <Player />
-            </Suspense>
-        </createContextFind.Provider>
+        //             <Switch>
+        //                 {/* {
+        //                     routes.map(item => {
+        //                         const { path, exact = true, component: C } = item;
+        //                         return <Route
+        //                             key={path}
+        //                             path={path}
+        //                             exact={exact}
+        //                             // component={C}
+        //                             render={(props) => {
+        //                                 // props.history.title = item.title
+        //                                 // window.reactMusicTitle = item.title
+        //                                 setHeaterTitle(item)
+        //                                 return <C {...props} key={item.title} title={item.title} />
+        //                             }}
+        //                         />
+        //                     })
+        //                 } */}
+        //                 <Route path="*" render={(props) => {
+        //                     props.history.push({ pathname: "/find", })
+        //                 }} />
+        //             </Switch>
+        //         </HashRouter>
+        //         <Player />
+        //     </Suspense>
+        // </createContextFind.Provider>
     )
 }
 
