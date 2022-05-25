5c9947f8362b7b4a4fcfe83739496d7b69e7a7a1
useryize <136076063@qq.com>
2022-05-25 15:30:33 +0800

    react-router-dom v6

--- src/app.js
+++ src/app.js
@@ -1,59 +1,44 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes  } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import createContextFind from './hooks/App/createContextApp'
 import { reducer, initialState } from './hooks/App/useReducerApp'
-import history from './utils/history';
-import Aaa from './components/Player'
-// import routes from './routes'
-const { lazy, Suspense, useReducer, useState } = React;
+// import history from './utils/history';
+import routesDom from './routes'
+const {
+    lazy, Suspense,
+    useReducer,
+    // useState
+} = React;
 
 const Player = lazy(() => import('./components/Player'));
 const Header = lazy(() => import('./components/Header/header'));
 
 const App = () => {
     const [state, dispatch] = useReducer(reducer, initialState)
-    const [heaterTitle, setHeaterTitle] = useState(null)
+    // const [heaterTitle, setHeaterTitle] = useState(null)
     return (
-		<Router>
-			<Routes>
-				<Route path='home' element={<Aaa></Aaa>}></Route>
-				<Route path='*' element={<Aaa></Aaa>}></Route>
-			</Routes>
-		</Router>
-        // <createContextFind.Provider value={{ state, dispatch }}>
-        //     <Suspense fallback={
-        //         null
-        //         // <div>Loading...</div> 
-        //     }>
-        //         <Header heaterTitle={heaterTitle} />
-        //         <HashRouter history={history}>
 
-        //             <Switch>
-        //                 {/* {
-        //                     routes.map(item => {
-        //                         const { path, exact = true, component: C } = item;
-        //                         return <Route
-        //                             key={path}
-        //                             path={path}
-        //                             exact={exact}
-        //                             // component={C}
-        //                             render={(props) => {
-        //                                 // props.history.title = item.title
-        //                                 // window.reactMusicTitle = item.title
-        //                                 setHeaterTitle(item)
-        //                                 return <C {...props} key={item.title} title={item.title} />
-        //                             }}
-        //                         />
-        //                     })
-        //                 } */}
-        //                 <Route path="*" render={(props) => {
-        //                     props.history.push({ pathname: "/find", })
-        //                 }} />
-        //             </Switch>
-        //         </HashRouter>
-        //         <Player />
-        //     </Suspense>
-        // </createContextFind.Provider>
+        <createContextFind.Provider value={{ state, dispatch }}>
+            <Suspense fallback={
+                null
+                // <div>Loading...</div> 
+            }>
+
+                <Router>
+                    <Header />
+                    <Routes>
+                        {
+                            routesDom.map(item => {
+                                const { path, element: E } = item
+                                return <Route key={path} path={path} element={<E />}></Route>
+                            })
+                        }
+                        <Route path='*' element={<Navigate to="/find" />}></Route>
+                    </Routes>
+                </Router>
+                <Player />
+            </Suspense>
+        </createContextFind.Provider>
     )
 }
 
