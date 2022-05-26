d724ecf3c643e533d9c3733654cb77ce087f2a31
useryize <136076063@qq.com>
2022-05-26 10:00:32 +0800

    header

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,7 +1,7 @@
 import React from 'react'
 // import history from '../../utils/history'
 import { useNavigate } from "react-router-dom";
-import { NavBar, Popup, Button, Avatar, Input } from 'antd-mobile'
+import { NavBar, Popup, Button, Avatar } from 'antd-mobile'
 import styles from './index.module.less'
 
 const {
