2c0c503b5af06eeedf4146cf00652785955db155
useryize <136076063@qq.com>
2022-05-27 15:19:13 +0800

    获取歌单所有歌曲

--- src/hooks/SongSheetDetails/useReducerSong.js
+++ src/hooks/SongSheetDetails/useReducerSong.js
@@ -1,9 +1,11 @@
 import { axiosGet } from '../../utils/axios';
-import { playlistDetail } from '../../utils/apis';
+import { playlistDetail, playlistTrackAll } from '../../utils/apis';
 const PLAY_LIST_DETAIL = 'PLAY_LIST_DETAIL';
+const PLAY_LIST_TRACK_ALL = 'PLAY_LIST_TRACK_ALL';
 
 export const initialState = {
     songList: {},
+	songAllList: {}
 }
 
 export const reducer = (state = initialState, action) => {
@@ -11,6 +13,12 @@ export const reducer = (state = initialState, action) => {
         return {
             ...state,
             songList: action.songList,
+        }
+    }
+    if (action.type === PLAY_LIST_TRACK_ALL) {
+        return {
+            ...state,
+            songAllList: action.songAllList,
         }
     }
     return state;
@@ -27,6 +35,20 @@ export const getPlaylistDetail = ({ dispatch, params } = {}) => {
         dispatch({
             type: PLAY_LIST_DETAIL,
             songList: res || {}
+        })
+    })
+    return axiosRes
+};
+// 获取歌单所有歌曲
+export const getPlaylistTrackAll = ({ dispatch, params } = {}) => {
+    const axiosRes = axiosGet({
+        url: playlistTrackAll,
+        params
+    })
+    axiosRes.then((res) => {
+        dispatch({
+            type: PLAY_LIST_TRACK_ALL,
+            songAllList: res || {}
         })
     })
     return axiosRes
