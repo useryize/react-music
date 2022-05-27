fd5e0bd4c9f4cccf6b44fb339df6c204463b289c
useryize <136076063@qq.com>
2022-05-27 15:18:56 +0800

    获取所有歌曲

--- src/pages/SongSheetDetails/SongList/index.js
+++ src/pages/SongSheetDetails/SongList/index.js
@@ -1,34 +1,38 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-import createContextSong from '@/hooks/SongSheetDetails/createContextSong'
-import { getPlaylistDetail } from '@/hooks/SongSheetDetails/useReducerSong'
+import createContextSong from '../../../hooks/SongSheetDetails/createContextSong'
+import { getPlaylistDetail, getPlaylistTrackAll } from '../../../hooks/SongSheetDetails/useReducerSong'
 import SongListPublic from '../../../components/SongListPublic'
 import { Image } from 'antd-mobile'
 import styles from './index.module.less'
 const {
     useContext,
     // useEffect,
-    useEffect,
-    memo
+    useEffect
 } = React
 const SongListCom = () => {
     const useParamsRoute = useParams()
     const {
         state: {
             songList: {
-                playlist: {
-                    tracks = []
-                } = {},
+                // playlist: {
+                // 	tracks = []
+                // } = {},
                 playlist = {}
             } = {},
+            songAllList: {
+                songs = []
+            } = {}
         } = {}, dispatch
     } = useContext(createContextSong)
 
     useEffect(() => {
         // 歌单列表
         const { id = '' } = useParamsRoute
-        console.error(id);
         getPlaylistDetail({ dispatch, params: { id } })
+
+        // 获取所有歌曲
+        getPlaylistTrackAll({ dispatch, params: { id, offset: 0, limit: 1000 } })
     }, [])
 
     return (
@@ -58,9 +62,9 @@ const SongListCom = () => {
                 </div>
 
             </div>
-            <SongListPublic dataInfo={tracks} />
+            <SongListPublic dataInfo={songs} />
         </>
     )
 }
 
-export default memo(SongListCom)
+export default SongListCom
