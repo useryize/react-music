f3d65acf7d818fbf87b12a8448164ebbee1895c3
useryize <136076063@qq.com>
2022-05-27 18:03:39 +0800

    SearchInput

--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -3,7 +3,7 @@ import createContextSearch from '../../hooks/Search/createContextSearch'
 import { reducer, initialState } from '../../hooks/Search/useReducerSearch'
 
 const KeywordTabs = lazy(() => import('./KeywordTabs/index'));
-const SearchInput = lazy(() => import('./SearchInput/index'));
+// const SearchInput = lazy(() => import('./SearchInput/index'));
 const SearchList = lazy(() => import('./SearchList/index'));
 
 const Search = (props) => {
@@ -11,7 +11,7 @@ const Search = (props) => {
     return (
         <createContextSearch.Provider value={{ state, dispatch, props }}>
             <KeywordTabs />
-            <SearchInput />
+            {/* <SearchInput /> */}
             <SearchList />
         </createContextSearch.Provider>
     )
