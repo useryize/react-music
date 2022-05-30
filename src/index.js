074f64b7f1ce308197bbe898eb2bd5f0427b1d79
useryize <136076063@qq.com>
2022-05-30 17:15:06 +0800

    icon在线资源

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import App from './app';
 import './utils/flexible';
 import './assets/style/global.module.css';
 // import './assets/iconfont/iconfont.css'
-import 'https://at.alicdn.com/t/font_3420473_4hc5n5rj5hg.css?spm=a313x.7781069.1998910419.86&file=font_3420473_4hc5n5rj5hg.css'
 import VConsole from 'vconsole';
 import 'antd-mobile/es/global'
 new VConsole();
