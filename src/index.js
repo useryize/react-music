543d88340386cb05dc72b08bd497b33d466aca76
useryize <136076063@qq.com>
2022-05-30 17:07:06 +0800

    iconfont在线资源

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './app';
 import './utils/flexible';
 import './assets/style/global.module.css';
-import './assets/iconfont/iconfont.css'
+// import './assets/iconfont/iconfont.css'
+import 'https://at.alicdn.com/t/font_3420473_4hc5n5rj5hg.css?spm=a313x.7781069.1998910419.86&file=font_3420473_4hc5n5rj5hg.css'
 import VConsole from 'vconsole';
 import 'antd-mobile/es/global'
 new VConsole();
